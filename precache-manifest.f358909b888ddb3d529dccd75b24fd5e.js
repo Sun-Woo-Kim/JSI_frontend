bb3b9333dc19ce81477cc102fab2921674ef4faa
Harry Kim <rlatjsdn1261@naver.com>
2019-07-28 01:11:48 +0900

    Updates

---
+++ precache-manifest.f358909b888ddb3d529dccd75b24fd5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15005f08e1707fc96544f412a53e321c",
+    "url": "/JSI_frontend/index.html"
+  },
+  {
+    "revision": "a290b4864c01ca4bc255",
+    "url": "/JSI_frontend/static/css/main.189772de.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/JSI_frontend/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "a290b4864c01ca4bc255",
+    "url": "/JSI_frontend/static/js/main.ae653b09.chunk.js"
+  },
+  {
+    "revision": "13d8ff7cdd537b2b8dd9",
+    "url": "/JSI_frontend/static/js/runtime~main.92df373d.js"
+  }
+]);
